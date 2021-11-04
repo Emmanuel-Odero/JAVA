2f42aa59eceadd5cbefa6055891673dd754dc98a
unknown <emmanuelodero808@gmail.com>
2021-11-04 20:43:25 +0300

    made additional note on the learning folder

---
+++ LEARNING/java_comments.java
@@ -0,0 +1,9 @@
+public class java_comments {
+    public static void main(String[ ] args){
+        System.out.println("Coments are used to add notes to your java code and making them more readable");
+    //This is a single line comment it wont be printed to the screen
+    //single line comments are created using '//' at the beginning of a line
+    /*This is a multi line comment*/
+    //it is creates using the '/*' symbol at the beginnig of the statements you want to comment and end with the '*/' symbol at the end of the comment
+    }
+}

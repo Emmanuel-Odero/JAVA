9426aac54545b6e6df43ef9e607088a63812790a
unknown <emmanuelodero808@gmail.com>
2021-11-05 00:12:04 +0300

    another addition to the folder learning

---
+++ LEARNING/math.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+   public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+       //get salaries
+       int salary1 = scanner.nextInt();
+       int salary2 = scanner.nextInt();
+       //your code goes here
+       System.out.println(salary1+salary2);
+
+   }
+}

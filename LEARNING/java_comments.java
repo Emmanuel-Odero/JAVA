4c677820ba8937b1c6e94ea13a4c8d70b72f9f18
unknown <emmanuelodero808@gmail.com>
2021-11-09 20:19:03 +0300

    Learning the java logical operators

--- LEARNING/java_comments.java
+++ LEARNING/java_comments.java
@@ -1,6 +1,6 @@
-public class java_comments {
-    public static void main(String[ ] args){
-        System.out.println("Coments are used to add notes to your java code and making them more readable");
+// public class java_comments {
+//     public static void main(String[ ] args){
+//         System.out.println("Coments are used to add notes to your java code and making them more readable");
     //This is a single line comment it wont be printed to the screen
     //single line comments are created using '//' at the beginning of a line
     /*This is a multi line comment*/
@@ -10,5 +10,5 @@ public class java_comments {
 
     /** This is also a
     documentation comment */
-    }
-}
+//     }
+// }
--- LEARNING/logical_operators.java
+++ LEARNING/logical_operators.java
@@ -19,3 +19,46 @@
     }
 }
 */
+/**the logical AND operator(&&)
+ * the logical&& operator could be used to easen task especially where there are multiple if statements
+ * */
+// import java.util.Scanner;
+// public class andOperator{
+//      public static void main(String[] args){
+//          Scanner userAge= new Scanner(System.in);
+//          Scanner userName= new Scanner(System.in);
+//          int age = userAge.nextInt();
+//          String name= userName.nextLine();
+
+//          if(age>=18 && name==name){
+//              System.out.println("Welcome Back " + name);
+//          }else if (age<18){
+//              System.out.println("You are under age");
+//          }else{
+//              System.out.println("Please check that your name is correct");
+//          }
+//      }
+//  }
+/**
+ * **************The OR Operator***********************
+ * The OR operator (||) checks if any one of the conditions is true.
+ * The condition becomes true, if any one of the operands evaluates to true.
+ * For example:
+ */
+public class App{
+public static void main(String[] args){
+    // int age =25;
+    // int money =100;
+    // if(age>18 || money>=500){
+    //     System.out.println("Welcome");
+    //     }
+    int a = 11; int b = 12;
+    int c = 40;
+    if (a > 100 || b > 3) {
+    System.out.println(a); 
+    } else {
+    System.out.println(c);
+    }
+    //
+    }
+}

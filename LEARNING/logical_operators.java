23c182c1123db50d6b8ee29d75d5fb2eb433af1c
unknown <emmanuelodero808@gmail.com>
2021-11-09 17:04:54 +0300

    another learning process

---
+++ LEARNING/logical_operators.java
@@ -0,0 +1,21 @@
+/**
+ * Logical operators are used to combine multiple conditions.
+    Let's say you wanted your program to output "Welcome!" only when the variable age is greater than 18 and the variable money is greater than 500.
+    One way to accomplish this is to use nested if statements:
+ */
+/**public class logical_operators {
+    public static void main(String[] args){
+        int age = 12;
+        int money = 701;
+        if(age>18){
+            if(money>500){
+                System.out.println("Welcome");
+            }else {
+                System.out.println("please check you balance!!!");
+            }
+        }else{
+            System.out.println("You are too young");
+        }
+    }
+}
+*/

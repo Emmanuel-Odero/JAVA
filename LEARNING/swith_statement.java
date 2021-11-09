e99de8beaf77f3444aeffb01cc1cd61ab15b5550
unknown <emmanuelodero808@gmail.com>
2021-11-09 21:00:30 +0300

    Learning the java switch statements

---
+++ LEARNING/swith_statement.java
@@ -0,0 +1,19 @@
+/**
+ * A switch statement tests a variable for equality against a list of values
+ * Each value is called a case, and the variable being switched on is checked for each case.
+ * -------------Syntax of Switch statement:-------------
+ * switch (expression) {
+   case value1 :
+     Statements
+     break; optional
+   case value2 :
+     Statements
+     break; optional
+    You can have any number of case statements.
+    default : Optional
+       Statements
+}
+ */
+public class swith_statement {
+    
+}

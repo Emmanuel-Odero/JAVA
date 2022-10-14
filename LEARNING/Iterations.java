086aa0a6cc42212490b1f06f4c5f4437b15fcabc
Emmanuel-Odero <emmanuelodero808@gmail.com>
2022-10-14 11:25:16 +0000

    Iterators

---
+++ LEARNING/Iterations.java
@@ -0,0 +1,14 @@
+public class Iterations
+{
+    public static void main(String[] args)
+    {
+        // Using the for loop
+        for (int i = 1; i < 100; i++){
+            if (i % 2 == i)
+            {
+                System.out.println(i);
+                }
+        }
+        // the while loop
+    }
+}

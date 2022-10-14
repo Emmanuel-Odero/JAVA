80d0a4e94e586dcd18258a0398812153bb7d7d1e
Emmanuel-Odero <emmanuelodero808@gmail.com>
2022-10-14 10:51:14 +0000

    operators

---
+++ LEARNING/Operators.java
@@ -0,0 +1,14 @@
+public class Operators
+{
+    public static void main(String[] args){
+        double valueA = 10;
+        int valueB = 20;
+        String valueC = "5";
+        int ValueD = 5;
+
+        System.out.println("Results: " + (valueA * valueB));
+        System.out.println("Results: " + (valueC == ValueD));
+
+        
+    }
+}

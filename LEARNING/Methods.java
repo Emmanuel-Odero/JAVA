a9a8348f81cd5afef3c99fb1cc2bd174157f6a55
Emmanuel-Odero <emmanuelodero808@gmail.com>
2022-10-24 09:04:27 +0000

    java

---
+++ LEARNING/Methods.java
@@ -0,0 +1,12 @@
+public class Methods
+{
+    public static void main(String[] args){
+        return myThirdMethod();
+    }
+    static void myFistMethod( String valueA){
+
+    }
+    String myThirdMethod(){
+        return "12";
+    }
+}
